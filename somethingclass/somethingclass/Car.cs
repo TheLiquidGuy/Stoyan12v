ddbad72240ebad870f3696382de0f3c88d499542
TheLiquidGuy <stoqntran@abv.bg>
2018-03-30 09:29:47 +0300

    class
    class

---
+++ somethingclass/somethingclass/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace somethingclass
+{
+    class Car
+    {
+        public int ImmobilizerId { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public Array[] Tyre = new Array[4];
+        public IgnitionKey IgnitionKey { set; get; }
+        public Engine Engine { set; get; }
+        public Human Driver { set; get; }
+    }
+}
